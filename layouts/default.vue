88463172bb1b3d8139ef0f2905caedd0966ef51b
Ben Hong <ben@bencodezen.io>
2021-10-19 13:31:06 -0400

    feature: add basic navigation and layout

---
+++ layouts/default.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref } from 'vue'
+
+const navLinks = ref([
+  {
+    label: 'Home',
+    url: '/'
+  },
+  {
+    label: 'Signup',
+    url: '/signup'
+  },
+  {
+    label: 'TodoMVC',
+    url: '/todomvc'
+  }
+])
+</script>
+
+<template>
+  <main>
+    <nav>
+      <ul class="nav-list">
+        <li v-for="navItem in navLinks" :key="navItem.url">
+          <nuxt-link :to="navItem.url">{{ navItem.label }}</nuxt-link>
+        </li>
+      </ul>
+    </nav>
+    <slot></slot>
+  </main>
+</template>
+
+<style scoped>
+.nav-list {
+  display: flex;
+  list-style: none;
+  padding: 1rem;
+  margin: 0;
+}
+
+.nav-list li {
+  margin-right: 1rem;
+}
+</style>
---
+++ pages/todomvc.vue
@@ -0,0 +1,9 @@
+<script>
+export default {}
+</script>
+
+<template>
+  <h1>TodoMVC</h1>
+</template>
+
+<style></style>
